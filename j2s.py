cf10e3b42426def8c3373d921ca36f5b15d2ba4b
udaykumar <Uday.crazyuk@gmail.com>
2021-11-23 14:00:06 -0800

    .

--- j2s.py
+++ j2s.py
@@ -11,3 +11,26 @@ a, b = 0, 1
 while b < 100:
     print(b),
     a, b = b, a+b
+
+X = 4
+Y = "This is a string "
+print(X)
+print (Y)
+
+pi = 3.14
+print(pi)
+one = 1
+two = 2
+three = 3
+print(one)
+print(two)
+print(three)
+two = 4
+print(two)
+print(one)
+
+decimal = 2.1
+print(decimal)
+
+StringVar = "Hello" + "1"
+print(StringVar)
